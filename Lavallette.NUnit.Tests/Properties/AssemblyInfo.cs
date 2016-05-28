33842d4f129fb07a8c7efb0950f83641e19cc849
Don Demsak <don@donxml.com>
2016-05-28 16:12:58 -0400

    Added Working Test Projust with NUnit 3

--- Lavallette.NUnit.Tests/Properties/AssemblyInfo.cs
+++ Lavallette.NUnit.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lavallette.NUnit")]
+[assembly: AssemblyTitle("Lavallette.NUnit.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lavallette.NUnit")]
+[assembly: AssemblyProduct("nunit.tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("067456c0-086c-46a8-b37f-1405717b7bfc")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Test.DataAccess/MyConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Test.DataAccess
+{
+    public class MyConnection
+    {
+        public MyConnection()
+        {
+
+        }
+
+        public void OpenConnection(SqlConnection connection)
+        {
+            connection.Open();
+        }
+    }
+}
