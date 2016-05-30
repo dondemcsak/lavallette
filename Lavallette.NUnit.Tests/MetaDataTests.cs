c3a595321eaa7864fca9c7d9e74da65b4ac948a9
Don Demsak <don@donxml.com>
2016-05-30 15:36:52 -0400

    First working, simple test

--- Lavallette.NUnit.Tests/MetaDataTests.cs
+++ Lavallette.NUnit.Tests/MetaDataTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using Lavallette;
 
 namespace Lavallette.NUnit.Tests
@@ -11,11 +12,24 @@ namespace Lavallette.NUnit.Tests
     [TestFixture]
     public class MetaDataTests
     {
-        [Test]
-        public void TestMethod()
+        Assembly assembly;
+
+        [OneTimeSetUp]
+        public void TestSetup()
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            Console.Write(Assembly.GetExecutingAssembly().Location);
+            string currentPath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            assembly = Assembly.LoadFrom(currentPath + @"\Test.DataAccess.dll");
+            Assert.IsNotNull(assembly);
+        }
+
+        [Test]
+        public void CanGetReferencedAssemblyNames()
+        {
+            var tartgetAssembly = new TargetAssembly(assembly);
+            Assert.IsNotNull(tartgetAssembly);
+            Assert.True(tartgetAssembly.RefereenceAssemblyNames.Count() > 0);
+
         }
     }
 }
